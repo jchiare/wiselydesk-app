5e24580406838464b883744cca5844107284f5db
Jay Chiarella <jay.gch93@gmail.com>
2023-09-01 15:28:47 +0200

    add linting prettier tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
@@ -10,11 +11,23 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        sans: ['InterVariable', 'Inter', ...defaultTheme.fontFamily.sans],
+      },
+      animation: {
+        'assistant-message': 'blink 1s steps(5,start) infinite',
+      },
+      keyframes: {
+        blink: {
+          to: {
+            visibility: 'hidden',
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins:  [require('@tailwindcss/typography')],
 }
 export default config
