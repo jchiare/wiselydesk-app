c1bb82321651d34ec01bd4bf57984030298bb25f
Jay Chiarella <jay.gch93@gmail.com>
2023-09-07 16:45:22 +0200

    improve navigation

--- components/web/side-nav.tsx
+++ components/web/side-nav.tsx
@@ -1,36 +1,14 @@
-import {
-  ChatBubbleBottomCenterTextIcon,
-  ChartBarSquareIcon
-} from "@heroicons/react/24/outline";
-import { concatClassNames } from "@/lib/shared/utils";
 import Image from "next/image";
 import BotSelection from "@/components/web/bot-selection";
-import SideNavDisclosure from "@/components/web/side-nav-disclosure";
-import { Session } from "next-auth";
 
-const navigation = [
-  {
-    name: "Conversations",
-    icon: ChatBubbleBottomCenterTextIcon,
-    href: "/bot/conversations",
-    children: null
-  },
-  {
-    name: "Analytics",
-    icon: ChartBarSquareIcon,
-    href: "/bot/analytics",
-    children: null
-  }
-];
+import { Session } from "next-auth";
+import Navigation from "@/components/web/navigation";
 
 export default async function SideNav({
   session
 }: {
   session: Session;
 }): Promise<JSX.Element> {
-  const currentItem = navigation.find(
-    (item) => item.href === "/bot/conversations" || item.href === "/bot/"
-  );
   return (
     <div className="flex w-64 grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-gray-800 px-6">
       <div className="flex h-16 flex-col items-center">
@@ -42,30 +20,7 @@ export default async function SideNav({
       <nav className="flex flex-1 flex-col">
         <ul role="list" className="flex flex-1 flex-col gap-y-7">
           <li>
-            <ul role="list" className="-mx-2 space-y-1">
-              {navigation.map((item) => (
-                <li key={item.name}>
-                  {!item?.children ? (
-                    <a
-                      href={item.href}
-                      className={concatClassNames(
-                        item.href === currentItem?.href
-                          ? "bg-gray-700"
-                          : "hover:bg-gray-700",
-                        "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-400"
-                      )}>
-                      <item.icon
-                        className="h-6 w-6 shrink-0 text-gray-400"
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                  ) : (
-                    <SideNavDisclosure currentItem={currentItem} item={item} />
-                  )}
-                </li>
-              ))}
-            </ul>
+            <Navigation />
           </li>
           <li className="-mx-6 mt-auto">
             <hr className="border text-gray-400" />
