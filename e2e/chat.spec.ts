ae70a3931eb0c21540a64cfff166ba85d5823785
Jay Chiarella <jay.gch93@gmail.com>
2023-09-15 15:33:40 +0200

    add amboss de welcome message

--- e2e/chat.spec.ts
+++ e2e/chat.spec.ts
@@ -10,3 +10,14 @@ test("should navigate to the chat page for amboss en", async ({ page }) => {
     "Hi there and welcome to AMBOSS! I'm a virtual assistant and happy to help you with your request."
   );
 });
+
+test("should navigate to the chat page for amboss de", async ({ page }) => {
+  // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  await page.goto(
+    "/chat/amboss?locale=de&client_api_key=hYn1picbsJfRm6vNUMOKv1ANYFSD4mZNTgsiw7LdHnE&create_support_ticket=true"
+  );
+  // Welcome message loaded
+  await expect(page.getByRole("paragraph")).toContainText(
+    "Hallo und herzlich Willkommen bei AMBOSS! Ich bin ein virtueller Assistent und stehe dir bei Fragen gerne zur Verfügung!"
+  );
+});
