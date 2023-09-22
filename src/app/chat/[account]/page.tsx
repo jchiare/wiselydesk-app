0d65ef7d080aaa886a70be25a03a58da4a8c9f92
Jay Chiarella <jay.gch93@gmail.com>
2023-09-22 15:31:41 +0200

    fix

--- src/app/chat/[account]/page.tsx
+++ src/app/chat/[account]/page.tsx
@@ -1,6 +1,7 @@
 import Chat, { type SearchParams } from "@/components/chat";
 import getChatTheme from "@/lib/chat/chat-theme";
 import { NEXTJS_BACKEND_URL } from "@/lib/shared/constants";
+import type { Bot } from "@prisma/client";
 
 type ChatPageProps = {
   params: {
@@ -9,12 +10,17 @@ type ChatPageProps = {
   searchParams: SearchParams;
 };
 
-export default async function Page({ params, searchParams }: ChatPageProps) {
-  const bot = await fetch(`${NEXTJS_BACKEND_URL}/api/bot/find`, {
+async function getBot(clientApiKey: string) {
+  const response = await fetch(`${NEXTJS_BACKEND_URL}/api/bot/find`, {
     method: "POST",
-    cache: "no-cache"
+    body: JSON.stringify({ clientApiKey })
   });
+  const bot = await response.json();
+  return bot as Bot;
+}
 
+export default async function Page({ params, searchParams }: ChatPageProps) {
+  const bot = await getBot(searchParams.client_api_key);
   const chatTheme = getChatTheme(params.account);
   return (
     <main>
