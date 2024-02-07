860fdd9c3d6d14438a6685293159208104ee9ea9
Jay Chiarella <jay.gch93@gmail.com>
2024-02-07 10:54:15 +0100

    hide viewing type

--- components/web/analytics/chart-viewing-type-selector.tsx
+++ components/web/analytics/chart-viewing-type-selector.tsx
@@ -32,7 +32,7 @@ export default function ChartViewingTypeSelector({
     <Listbox value={viewingType} onChange={changeViewingType}>
       {({ open }) => (
         <>
-          <div className="mt-2">
+          <div className="mt-2 hidden">
             <Listbox.Button className="relative cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:cursor-pointer focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
               <span className="block truncate">{viewingType}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
