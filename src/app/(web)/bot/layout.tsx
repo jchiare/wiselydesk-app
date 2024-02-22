123dac76e7350a276ffe11e9198e27062b357a01
Jay Chiarella <jay.gch93@gmail.com>
2024-02-22 20:52:45 +0100

    add posthog

--- src/app/(web)/bot/layout.tsx
+++ src/app/(web)/bot/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata } from "next";
-import SessionProvider from "@/src/app/(web)/bot/providers";
+import SessionProvider, { PHProvider } from "@/src/app/(web)/bot/providers";
 import SideNav from "@/components/web/side-nav";
 import { fetchServerSession } from "@/lib/shared/auth";
+import dynamic from "next/dynamic";
 
-export const dynamic = "force-dynamic";
+const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
+  ssr: false
+});
 
 export const metadata: Metadata = {
   title: "WiselyDesk App",
@@ -19,12 +22,14 @@ export default async function WebLayout({
 
   return (
     <SessionProvider session={session}>
-      <div className="m-0 flex h-screen w-full bg-gray-100 p-0">
-        <SideNav session={session} />
-        <main className=" w-[calc(100%_-_18rem)] flex-grow overflow-y-scroll">
-          {children}
-        </main>
-      </div>
+      <PHProvider>
+        <div className="m-0 flex h-screen w-full bg-gray-100 p-0">
+          <SideNav session={session} />
+          <main className=" w-[calc(100%_-_18rem)] flex-grow overflow-y-scroll">
+            {children}
+          </main>
+        </div>
+      </PHProvider>
     </SessionProvider>
   );
 }
--- src/app/(web)/bot/providers.tsx
+++ src/app/(web)/bot/providers.tsx
@@ -1,6 +1,19 @@
 "use client";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import { SessionProvider } from "next-auth/react";
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    capture_pageview: false // Disable automatic pageview capture, as we capture manually
+  });
+}
+
+export function PHProvider({ children }: { children: React.ReactNode }) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
 
 export default function Providers({
   children,
