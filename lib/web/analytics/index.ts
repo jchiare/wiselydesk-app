250f35ad74556931b71149870275674af8dd9e8a
Jay Chiarella <jay.gch93@gmail.com>
2024-03-16 07:56:30 +0100

    add onthly

--- lib/web/analytics/index.ts
+++ lib/web/analytics/index.ts
@@ -111,58 +111,30 @@ export class Analytics {
   async getMonthlyEscalationCounts(
     botId: string,
     today: Date
-  ): Promise<ConversationAnalyticsData[]> {
-    const oneHundredTwentyDaysAgo = Math.floor(
-      new Date(today).setDate(today.getDate() - 120) / 1000
+  ): Promise<EscalationAnalyticsData[]> {
+    const oct1st2023 = Math.floor(
+      new Date("202310-01T00:00:00Z").getTime() / 1000
     );
 
-    const conversations = await this.prisma.$queryRaw<RawConversationData[]>`
-      SELECT
-        COUNT(*) AS total_convo_count,
-        SUM(CASE WHEN ticket_deflected = TRUE THEN 1 ELSE 0 END) AS deflected_convo_count,
-        SUM(CASE WHEN zendesk_ticket_url IS NOT NULL THEN 1 ELSE 0 END) AS ticket_created_count,
-        date_format(created_at, '%b %Y') AS date,
-        SUM(CASE WHEN rating = 'Positive' THEN 1 ELSE 0 END) AS positive_count,
-        SUM(CASE WHEN rating = 'Negative' THEN 1 ELSE 0 END) AS negative_count
-      FROM
-      (
-      SELECT
-        c.id,
-        c.ticket_deflected,
-        c.zendesk_ticket_url,
-        date_format(c.created_at, '%b %e') AS date,
-        CASE
-          WHEN SUM(CASE WHEN m.is_helpful = 1 THEN 1 ELSE 0 END) > SUM(CASE WHEN m.is_helpful = 0 THEN 1 ELSE 0 END) THEN 'Positive'
-          WHEN SUM(CASE WHEN m.is_helpful = 1 THEN 1 ELSE 0 END) < SUM(CASE WHEN m.is_helpful = 0 THEN 1 ELSE 0 END) THEN 'Negative'
-          ELSE 'Neutral'
-        END AS rating,
-        c.bot_id bot_id,
-        c.created_at created_at
-      FROM
-        message m
-        JOIN conversation c ON m.conversation_id = c.id
-      WHERE
-        m.bot_id = ${botId} AND
-        UNIX_TIMESTAMP(m.created_at) >= ${oneHundredTwentyDaysAgo} AND
-        c.livemode = 1
-      GROUP BY
-        c.id, 4
-    ) AS conversation
-      WHERE
-        bot_id = ${botId} AND
-        UNIX_TIMESTAMP(created_at) >= ${oneHundredTwentyDaysAgo}
-      GROUP BY
-      4
+    const escalations = await this.prisma.$queryRaw<RawEscalationData[]>`
+        select 
+          reason, 
+          date_format(created_at, '%b %Y') AS date, 
+          count(id) "count" 
+        from escalation 
+        where 
+          bot_id = ${botId} AND
+          UNIX_TIMESTAMP(created_at) >= ${oct1st2023} and
+          livemode = 1
+        group by 
+          1, 2;
     `;
 
-    return conversations.map(conversation => ({
-      frequency_type: "monthly",
-      total_convo_count: Number(conversation.total_convo_count),
-      deflected_convo_count: Number(conversation.deflected_convo_count),
-      ticket_created_count: Number(conversation.ticket_created_count),
-      positive_count: Number(conversation.positive_count),
-      negative_count: Number(conversation.negative_count),
-      date: conversation.date
+    return escalations.map(escalation => ({
+      frequency_type: "daily",
+      count: Number(escalation.count),
+      reason: escalation.reason,
+      date: escalation.date
     }));
   }
 
