d80f0361d4e8df87f1ec9655c8ff5f7e32726ffd
Jay Chiarella <jay.gch93@gmail.com>
2024-03-31 21:39:21 +0200

    remove posthog

--- src/app/(web)/bot/layout.tsx
+++ src/app/(web)/bot/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
 import SideNav from "@/components/web/side-nav";
 import { fetchServerSession } from "@/lib/shared/auth";
 import { orgChooser } from "@/lib/shared/org-chooser";
 import prisma from "@/lib/prisma";
+import { unstable_cache } from "next/cache";
 
 export const dynamic = "force-dynamic";
 
@@ -24,19 +24,43 @@ async function getBots(orgId: number) {
   }
 }
 
-async function setActivity(orgId: number) {
-  if (orgId === 2) {
-    console.log("Skipping WiselyDesk user");
-    return;
-  }
-  await prisma.activity.create({
-    data: {
-      orgId,
-      action: "web_layout"
+async function findExistingActivity(userId: number, orgId: number) {
+  const fifteenMinutesAgo = new Date(Date.now() - 15 * 60 * 1000);
+  return prisma.activity.findMany({
+    where: {
+      userId: userId,
+      orgId: orgId,
+      action: "web_layout",
+      createdAt: {
+        gte: fifteenMinutesAgo
+      }
     }
   });
 }
 
+const getCachedExistingActivity = unstable_cache(
+  findExistingActivity,
+  ["activity_check"],
+  { revalidate: 900 }
+);
+
+async function setActivity(userId: number, orgId: number) {
+  if (userId === 10) {
+    console.log("Skipping WiselyDesk user");
+    return;
+  }
+
+  const existingActivities = await getCachedExistingActivity(userId, orgId);
+  if (existingActivities.length === 0) {
+    await prisma.activity.create({
+      data: {
+        userId: userId,
+        orgId: orgId,
+        action: "web_layout"
+      }
+    });
+  }
+}
 export default async function WebLayout({
   children
 }: {
@@ -44,19 +68,16 @@ export default async function WebLayout({
 }) {
   const session = await fetchServerSession();
   const orgId = orgChooser(session);
-
   const bots = await getBots(orgId);
 
-  await setActivity(orgId);
+  await setActivity(session.user.internal_user_id, orgId);
 
   return (
-    <SessionProvider session={session}>
-      <div className="m-0 flex h-screen w-full bg-gray-100 p-0">
-        <SideNav session={session} bots={bots} />
-        <main className=" w-[calc(100%_-_18rem)] flex-grow overflow-y-scroll">
-          {children}
-        </main>
-      </div>
-    </SessionProvider>
+    <div className="m-0 flex h-screen w-full bg-gray-100 p-0">
+      <SideNav session={session} bots={bots} />
+      <main className=" w-[calc(100%_-_18rem)] flex-grow overflow-y-scroll">
+        {children}
+      </main>
+    </div>
   );
 }
