f4bdebf246dd0a8df46b8073dc9f0e94f0aae8c8
Jay Chiarella <jay.gch93@gmail.com>
2024-04-06 08:51:51 +0200

    add table

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,43 +1,40 @@
-import type { Config } from "tailwindcss";
-import defaultTheme from "tailwindcss/defaultTheme";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/chat/**/*.{js,ts,jsx,tsx,mdx}",
-    "./lib/chat/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/web/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/shared/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/widget/**/*.{js,ts,jsx,tsx,mdx}"
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
       },
-      fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
-        "assistant-message": "blink 1s steps(5,start) infinite",
-        spin: "spin 1s linear infinite"
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
-      keyframes: {
-        blink: {
-          to: {
-            visibility: "hidden"
-          }
-        },
-        spin: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(360deg)" }
-        }
-      }
-    }
+    },
   },
-  plugins: [require("@tailwindcss/typography")]
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
