92093b34b81fd1198ebfe11c786ce96bcdde8dcc
Jay Chiarella <jay.gch93@gmail.com>
2024-06-23 22:00:17 +0200

    remove unused

--- lib/shared/services/llm/anthropic.ts
+++ lib/shared/services/llm/anthropic.ts
@@ -1,17 +1,9 @@
 import Anthropic from "@anthropic-ai/sdk";
-
-type AnthropicChat = {
-  inputTokens?: number;
-  outputTokens?: number;
-  fullResponse?: string;
-};
 export class AnthropicLLM {
   private client: Anthropic;
-  public chat: AnthropicChat;
 
   constructor() {
     this.client = new Anthropic();
-    this.chat = {};
   }
 
   async stream(
@@ -62,40 +54,5 @@ export class AnthropicLLM {
         break;
     }
     return undefined;
-  }
-
-  handleStreamEvent(event: Anthropic.Messages.RawMessageStreamEvent) {
-    switch (event.type) {
-      case "message_start":
-        this.chat = {
-          inputTokens: event.message.usage.input_tokens
-        };
-        break;
-      case "content_block_start":
-        if (event.content_block.type === "text") {
-          this.chat.fullResponse = event.content_block.text;
-          break;
-        }
-
-        throw new Error("Got somewhere we shouldn't be start");
-      case "content_block_delta":
-        if (event.delta.type === "text_delta") {
-          this.chat.fullResponse += event.delta.text;
-          break;
-        }
-
-        throw new Error("Got somewhere we shouldn't be delta");
-      case "message_delta":
-        this.chat.outputTokens = event.usage.output_tokens;
-        break;
-      default:
-        throw new Error(
-          `Got somewhere we shouldn't be ${JSON.stringify(event)}`
-        );
-        break;
-    }
-  }
-  public chatData() {
-    return this.chat;
   }
 }
