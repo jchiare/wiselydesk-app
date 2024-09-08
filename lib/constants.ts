da0c11b4ce6b851c4a8739a0e0b611360bbf46f6
Jay Chiarella <jay.gch93@gmail.com>
2024-09-08 08:24:55 +0200

    upload

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,8 +1,3 @@
-export const URL =
-  process.env.NODE_ENV === "development"
-    ? "http://0.0.0.0:5000"
-    : "https://wiselydesk-python-baackend.onrender.com";
-
 export const NEXTJS_BACKEND_URL =
   process.env.VERCEL_ENV === "preview"
     ? `https://${process.env.VERCEL_URL}`
