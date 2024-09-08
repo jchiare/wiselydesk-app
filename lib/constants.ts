a9c7bf1beb35ceaddfd86909f8182252c3d571e8
Jay Chiarella <jay.gch93@gmail.com>
2024-09-08 08:16:39 +0200

    fix url

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,6 +4,8 @@ export const URL =
     : "https://wiselydesk-python-baackend.onrender.com";
 
 export const NEXTJS_BACKEND_URL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3000"
-    : "https://apps.wiselydesk.com";
+  process.env.VERCEL_ENV === "preview"
+    ? `https://${process.env.VERCEL_URL}`
+    : process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://apps.wiselydesk.com";
