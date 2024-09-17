06bad2bce20a56b1cb04454c5085239cd70905a8
Jay Chiarella <jay.gch93@gmail.com>
2024-09-17 21:08:09 +0200

    improve prompts

--- lib/chat/prompts/tag-category.ts
+++ lib/chat/prompts/tag-category.ts
@@ -14,37 +14,36 @@ Also provide tags for the users information that you are think are relevant to t
 It's crucial to respond only in JSON format with the keys "ai_generated_tags", which will be snake cased tags you create, and "tags" which uses the tags given above. Don't explain your reasoning, just provide the JSON.
 `;
 
-// const TAG_CATEGORIES =
-//   "qbank_cancellation, library_membership_cancellation, all_membership_cancellation, bug_report, search, refund_outside_policy, refund_possibly_in_policy, article_content, question_bank_functionality, nejm_integration, article_functionality, question_bank, free_access_due_to_financial_hardship, study_plan_help, log_out_of_devices, b2b_sales_request, troubleshooting, feature_request, access_redemption_issue, request_for_agent";
-
 const AMBOSS_CATEGORIES = [
   {
     name: "billing",
     children: [
       "qbank_cancellation",
       "library_membership_cancellation",
-      "all_membership_cancellation"
+      "all_membership_cancellation",
+      "refund_request"
     ]
   },
   { name: "search", children: [] },
-  { name: "qbank", children: [] },
-  { name: "article", children: [] },
-  { name: "membership", children: [] },
-  { name: "support", children: [] },
-  { name: "NEJM", children: [] },
+  { name: "question_bank", children: ["functionality"] },
+  { name: "article", children: ["article_content", "functionality"] },
+  {
+    name: "membership",
+    children: [
+      "free_access_due_to_financial_hardship",
+      "access_redemption_issue",
+      "group_discount",
+      "b2b_sales_request"
+    ]
+  },
+  { name: "study_plan", children: [] },
+  { name: "support", children: ["request_for_agent", "bug_report", "contact"] },
+  { name: "NEJM", children: ["nejm_integration"] },
   { name: "medical_question", children: [] },
-  { name: "account_settings", children: [] },
-  { name: "mobile_apps", children: [] }
+  { name: "account_settings", children: ["log_out_of_devices"] },
+  { name: "mobile_apps", children: ["log_out_of_devices"] },
+  { name: "uncategorized", children: [] }
 ];
-
-// export const TAG_AMBOSS_CHATS_OLD = `
-// Please analyze the following customer service chat and provide the most appropriate tags based on the chats content and customer inquiry. All odd indexed messages are from the AI and even indexed messages are from the customer.
-// The tag should help categorize the email for efficient handling by the AMBOSS customer service team.
-// There are two categories of tags, one is the root/parent category that is more general and the other is the child category that is more specific. So a parent category could be 'billing' and the child category could be 'refund_request'.
-// There are two JSON keys to respond with: "tags" and "aiGeneratedTags". The value of each key should be an object with "parent" (string) and "childdren" (array of strings) keys respectively.
-// - For the "tags" key: Possible tags include:  ${AMBOSS_CATEGORIES}}. If none of the tags are relevant to the chat content, then tag the chat as 'uncategorized'.
-// - For the "aiGeneratedTags" key: Create tags based on the chat content that best categorizes the chat.
-//   `;
 
 export const TAG_AMBOSS_CHATS = `Please analyze the following customer service chat and categorize it using the most appropriate tags based on the chat's content and the customer's inquiry. The chat messages are indexed, with odd numbers representing messages from the AI and even numbers representing messages from the customer.
 
