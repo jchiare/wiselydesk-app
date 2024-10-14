95826bece2135e385470b554c12d4cff61db9bcf
Jay Chiarella <jay.gch93@gmail.com>
2024-10-14 09:55:43 +0200

    fix

--- next.config.cjs
+++ next.config.cjs
@@ -9,41 +9,29 @@ const nextConfig = {
       }
     ]
   },
-  experimental: {
-    // The instrumentation hook is required for Sentry to work on the serverside
-    instrumentationHook: true
+  async headers() {
+    return [
+      {
+        // matching business-hours API route for any bot id
+        source: "/api/bot/:anyid/business-hours",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://support.amboss.com"
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET"
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+          }
+        ]
+      }
+    ];
   }
 };
 
 module.exports = nextConfig;
-
-// Injected content via Sentry wizard below
-
-const { withSentryConfig } = require("@sentry/nextjs");
-
-module.exports = withSentryConfig(
-  module.exports,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-
-    org: "wiselydesk",
-    project: "wiselydesk-app"
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true
-  }
-);
