0be314eefa9ac1f06c630305bc0ebf24d5f2fcd8
Jay Chiarella <jay.gch93@gmail.com>
2024-10-14 10:11:57 +0200

    improve

--- next.config.cjs
+++ next.config.cjs
@@ -8,30 +8,6 @@ const nextConfig = {
         pathname: "**"
       }
     ]
-  },
-  async headers() {
-    return [
-      {
-        // matching business-hours API route for any bot id
-        source: "/api/bot/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://support.amboss.com"
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS"
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-          }
-        ]
-      }
-    ];
   }
 };
 
